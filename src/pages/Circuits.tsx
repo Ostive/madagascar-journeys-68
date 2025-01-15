a2a94bdc72329149b08b19b604231eadbbdce0ba
Ostive <ostivekevin@gmail.com>
2025-01-15 09:48:18 +0100

    col

--- src/pages/Circuits.tsx
+++ src/pages/Circuits.tsx
@@ -111,7 +111,7 @@ const CircuitsPage = () => {
           {isLoading ? (
             <div className="text-center py-8">Chargement des circuits...</div>
           ) : (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 justify-items-center">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center">
               {circuits?.map((circuit) => (
                 <CircuitCard key={circuit.id} circuit={circuit} />
               ))}
